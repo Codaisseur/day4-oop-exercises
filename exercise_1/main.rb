6fa9b5d219f8b7c57ebd344c7e95949cc5362745
michfarr <etnunc@protonmail.com>
2017-10-05 10:24:32 +0200

    Renamed files

--- exercise_1/main.rb
+++
@@ -1,9 +0,0 @@
-# ./main.rb
-require_relative 'space_ship'
-
-ship = SpaceShip.new('Millenium Falcon', 'gray')
-
-ship.print_details
-
-ship.paint('rainbow')
-ship.print_details
--- exercise_2/main.rb
+++
@@ -1,9 +0,0 @@
-# ./main.rb
-require_relative 'crew_member'
-
-chewie = CrewMember.new('Chewbacca', 'First Mate')
-
-chewie.print_credentials
-
-chewie.promote
-chewie.print_credentials
